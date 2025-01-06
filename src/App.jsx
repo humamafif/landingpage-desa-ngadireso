f6a7e1eb52a368256889cd3c2f0a7d215ebbed88
humamafif <afifalmahbubi04@gmail.com>
2025-01-06 11:56:43 +0700

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import About from "./components/About"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+    </>
+  )
+}
