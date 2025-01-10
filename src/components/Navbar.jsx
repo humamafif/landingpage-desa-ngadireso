20cd0e816d1b132a02ecd9039b3583ff4fb7b602
humamafif <afifalmahbubi04@gmail.com>
2025-01-10 13:05:13 +0700

    fix/Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import Logo from "./Logo";
 export default function Navbar() {
     return (
-        <nav className="fixed top-0 left-0 w-full bg-transparent z-20 my-6 lg:my-10 mx-4">
-            <div className="container mx-auto px-4 py-2 flex justify-between items-start">
+        <nav className="fixed top-0 left-0 w-full bg-transparent z-20 py-6 lg:py-4 px-4">
+            <div className="container mx-auto px-4 py-2 flex justify-between items-center">
                 <Logo />
                 <ul className="hidden space-x-10 lg:flex text-lg font-bold text-white text-shadow-md mr-8">
                     <li><a href="#" className="hover:text-gray-300">Home</a></li>
                     <li><a href="#about" className="hover:text-gray-300">About</a></li>
+                    <li><a href="#news" className="hover:text-gray-300">News</a></li>
                     <li><a href="#contact" className="hover:text-gray-300">Contact</a></li>
                 </ul>
             </div>
