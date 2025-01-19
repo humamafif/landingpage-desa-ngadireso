91889aab0baa201bbf433fcb875b461ad1909d20
humamafif <afifalmahbubi04@gmail.com>
2025-01-19 17:44:33 +0700

    feature/Catalog

---
+++ src/components/Catalog.jsx
@@ -0,0 +1,16 @@
+import Card from "./Card";
+
+export default function Catalog() {
+    return (
+        <div className="flex flex-col gap-4 lg:flex-row my-8 w-full lg:mx-12">
+            <div className="flex flex-col gap-4 h-full">
+                <Card src={"../../public/assets/images/kebun.png"} description={"Lorem ipsum dolor sit amet."} title={"Foto Sawah"} />
+                <Card src={"../../public/assets/images/karmel.png"} description={"Lorem ipsum dolor sit amet."} title={"Foto Karmel"} />
+            </div>
+            <div className="flex flex-col gap-4 h-full">
+                <Card src={"../../public/assets/images/kebun.png"} description={"Lorem ipsum dolor sit amet."} title={"Foto Sawah"} />
+                <Card src={"../../public/assets/images/karmel.png"} description={"Lorem ipsum dolor sit amet."} title={"Foto Karmel"} />
+            </div>
+        </div>
+    );
+}
