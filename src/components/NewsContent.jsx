aecec8450e3a78e0784bf6487cc89c9a1b4056b8
humamafif <afifalmahbubi04@gmail.com>
2025-01-19 17:47:37 +0700

    utils/News Content

---
+++ src/components/NewsContent.jsx
@@ -0,0 +1,37 @@
+export default function NewsContent() {
+    return (
+        <div className="flex flex-col gap-4">
+            <div className="w-screen lg:flex-row lg:gap-4 lg:px-16 flex flex-col items-center px-4 lg:items-start">
+                <img
+                    className="lg:w-1/2"
+                    src="../../public/assets/images/kebun.png" alt="" />
+                <div className="lg:w-1/2">
+                    <h2 className="uppercase font-extrabold text-2xl">judul berita</h2>
+                    <p className="lg:font-medium font-normal lg:text-base text-sm text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, magni excepturi recusandae dolor at deserunt maiores quis quos eius magnam est voluptatibus sapiente nemo corporis aliquam ex reiciendis eos sequi sed, quod alias aliquid.</p>
+                    <button className="bg-white text-black flex items-center gap-2 p-3 rounded-lg font-medium text-sm mt-3">Baca Selengkapnya <img src="../../public/assets/icons/arrow-right.svg" alt="" /></button>
+                </div>
+            </div>
+            <div className="w-screen lg:flex-row-reverse lg:gap-4 lg:px-16 flex flex-col items-center px-4 lg:items-start">
+                <img
+                    className="lg:w-1/2"
+                    src="../../public/assets/images/kebun.png" alt="" />
+                <div className="lg:w-1/2">
+                    <h2 className="uppercase font-extrabold text-2xl">judul berita</h2>
+                    <p className="lg:font-medium font-normal lg:text-base text-sm text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, magni excepturi recusandae dolor at deserunt maiores quis quos eius magnam est voluptatibus sapiente nemo corporis aliquam ex reiciendis eos sequi sed, quod alias aliquid.</p>
+                    <button className="bg-white text-black flex items-center gap-2 p-3 rounded-lg font-medium text-sm mt-3">Baca Selengkapnya <img src="../../public/assets/icons/arrow-right.svg" alt="" /></button>
+                </div>
+
+            </div>
+            <div className="w-screen lg:flex-row lg:gap-4 lg:px-16 flex flex-col items-center px-4 lg:items-start">
+                <img
+                    className="lg:w-1/2"
+                    src="../../public/assets/images/kebun.png" alt="" />
+                <div className="lg:w-1/2">
+                    <h2 className="uppercase font-extrabold text-2xl">judul berita</h2>
+                    <p className="lg:font-medium font-normal lg:text-base text-sm text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, magni excepturi recusandae dolor at deserunt maiores quis quos eius magnam est voluptatibus sapiente nemo corporis aliquam ex reiciendis eos sequi sed, quod alias aliquid.</p>
+                    <button className="bg-white text-black flex items-center gap-2 p-3 rounded-lg font-medium text-sm mt-3">Baca Selengkapnya <img src="../../public/assets/icons/arrow-right.svg" alt="" /></button>
+                </div>
+            </div>
+        </div>
+    )
+}   
