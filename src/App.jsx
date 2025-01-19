7aee894b267ef1dd4194e4aab5175c5f34a9224b
humamafif <afifalmahbubi04@gmail.com>
2025-01-19 17:53:28 +0700

    add/news & footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import About from "./components/About"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import News from "./components/News"
+import Footer from "./components/Footer"
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <About />
+      <News />
+      <Footer />
     </>
   )
 }
