7d1ee2f729acd4d387f0bdf48746835d4a2e1605
humamafif <afifalmahbubi04@gmail.com>
2025-01-19 17:49:22 +0700

    utils/Map

---
+++ src/components/Map.jsx
@@ -0,0 +1,15 @@
+export default function Map() {
+    return (
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15801.124753928765!2d112.77045224085161!3d-8.072776725566305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6249e7f59a7dd%3A0x557a198a5a25f4!2sNgadireso%2C%20Kec.%20Poncokusumo%2C%20Kabupaten%20Malang%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1734840839162!5m2!1sid!2sid"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            title="Google Maps Location"
+            className="rounded-xl shadow-2xl"
+
+        ></iframe>
+    )
+}
