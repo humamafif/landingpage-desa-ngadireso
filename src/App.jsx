9bc0b234f07981c9584c835a14bea9efc7507a2e
humamafif <afifalmahbubi04@gmail.com>
2025-01-21 23:24:38 +0700

    add/umkm section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Footer from "./components/Footer"
 import About from "./components/About"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import News from "./components/News"
-import Footer from "./components/Footer"
+import Umkm from "./components/Umkm";
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <About />
+      <Umkm />
       <News />
       <Footer />
     </>
