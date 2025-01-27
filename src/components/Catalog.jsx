65bdb16517e98e840019a5aaead7e3dd8f5b935f
humamafif <afifalmahbubi04@gmail.com>
2025-01-27 07:34:33 +0700

    fix/fix rasio & deskripsi + modal

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,16 +1,40 @@
+import { useState } from 'react';
+import data from "../core/data";
 import Card from "./Card";
+import Modal from './Modal';
 
 export default function Catalog() {
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+    const [popupData, setPopupData] = useState({ src: '', title: '', description: '' });
+
+    const handleCardClick = (src, title, description) => {
+        setPopupData({ src, title, description });
+        setIsPopupVisible(true);
+    };
+
+    const closePopup = () => {
+        setIsPopupVisible(false);
+    };
+
     return (
-        <div className="flex flex-col gap-4 lg:flex-row my-8 w-full lg:mx-12">
-            <div className="flex flex-col gap-4 h-full">
-                <Card src={"../../public/assets/images/kebun.png"} description={"Lorem ipsum dolor sit amet."} title={"Foto Sawah"} isDescriptionVisible={true} />
-                <Card src={"../../public/assets/images/karmel.png"} description={"Lorem ipsum dolor sit amet."} title={"Foto Karmel"} isDescriptionVisible={true} />
-            </div>
-            <div className="flex flex-col gap-4 h-full">
-                <Card src={"../../public/assets/images/kebun.png"} description={"Lorem ipsum dolor sit amet."} title={"Foto Sawah"} isDescriptionVisible={true} />
-                <Card src={"../../public/assets/images/karmel.png"} description={"Lorem ipsum dolor sit amet."} title={"Foto Karmel"} isDescriptionVisible={true} />
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-8 w-screen lg:px-14">
+            {data.slice(0, 4).map((card, index) => (
+                <div key={index} onClick={() => handleCardClick(card.src, card.title, card.description)}>
+                    <Card
+                        src={card.src}
+                        title={card.title}
+                        description={card.description}
+                        isDescriptionVisible={false}
+                    />
+                </div>
+            ))}
+            <Modal
+                isVisible={isPopupVisible}
+                onClose={closePopup}
+                src={popupData.src}
+                title={popupData.title}
+                description={popupData.description}
+            />
         </div>
     );
 }
