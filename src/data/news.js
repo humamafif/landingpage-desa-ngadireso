f094630b32d54e55513e29ce820b93bd8bec1316
humamafif <afifalmahbubi04@gmail.com>
2025-01-31 07:30:45 +0700

    feat/add data for news

---
+++ src/data/news.js
@@ -0,0 +1,25 @@
+const newsData = [
+    {
+        imageSrc: "/assets/images/moderasi_beragama.jpeg",
+        title: "Wujud Nyata Toleransi Beragama",
+        description: "KKM UIN Malang Desa Ngadireso melakukan kegiatan jaga malam misa natal yang bertempat di Pertapaan Karmel Desa Ngadireso, Kecamatan Poncokusumo, Kabupaten Malang pada hari Selasa (24/12/2024).",
+        link: "https://www.kompasiana.com/hisyamsyaroni0110/676c22ba34777c123c3b7414/wujud-nyata-toleransi-beragama-kelompok-kkm-uin-malang-jaga-malam-misa-natal-di-pertapaan-karmel-desa-ngadireso?utm_source=Whatsapp&utm_medium=Refferal&utm_campaign=Sharing_Desktop",
+        reverse: false
+    },
+    {
+        imageSrc: "/assets/images/edukasi_PHPBS.jpg",
+        title: "Edukasi Pencegahan Pernikahan Usia Dini dan PHBS untuk Generasi Sukses dan Sehat",
+        description: "KKM UIN Malang 9, 79, dan 149 Desa Ngadireso sukses menggelar sosialisasi tentang pencegahan pernikahan usia dini dan Pola Hidup Bersih dan Sehat (PHBS) di Balai Desa Ngadireso, Kecamatan Poncokusumo, Kabupaten Malang pada 18 Januari 2025.",
+        link: "https://uin-malang.ac.id/blog/post/read/25010103263002890",
+        reverse: true
+    },
+    {
+        imageSrc: "/assets/images/tri_dharma.jpeg",
+        title: "Sinergi Mahasiswa Dan Pemerintah Desa Melalui Program Kuliah Kerja Mahasiswa, Sebagai Perwujudan Tri Dharma Perguruan Tinggi.",
+        description: "Mahasiswa memiliki peran yang sangat besar dalam lingkungan masyarakat terutama kepada masyarakat desa. Dalam lingkungan akademisi, istilah tridharma perguruan tinggi merupakan faktor utama penentu kesuksesan sebuah perguruan tinggi.",
+        link: "https://www.kompasiana.com/hisyamsyaroni0110/67674ec5c925c43c3735d5c4/sinergi-mahasiswa-dan-pemerintah-desa-melalui-program-kuliah-kerja-mahasiswa-sebagai-perwujudan-tri-dharma-perguruan-tinggi?utm_source=Whatsapp&utm_medium=Refferal&utm_campaign=Sharing_Mobile",
+        reverse: false
+    }
+];
+
+export default newsData;
