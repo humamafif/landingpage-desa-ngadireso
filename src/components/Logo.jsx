dc6c8352acc42bf37fb747f369b4d8736893292d
humamafif <afifalmahbubi04@gmail.com>
2025-02-01 23:24:18 +0700

    fix/url image

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ export default function Logo() {
         <div className="text-lg font-bold flex items-center">
             <img
                 className="w-8 lg:w-20"
-                src="../../public/assets/images/logo_kabupaten_malang.png" alt="" />
+                src="/assets/images/logo_kabupaten_malang.png" alt="" />
             <div className="text-white font-extrabold lg:text-2xl text-xs ml-2">
                 <p className="text-shadow-md">Desa Ngadireso</p>
                 <p className="text-shadow-md">Poncokusumo - Kab.Malang</p>
